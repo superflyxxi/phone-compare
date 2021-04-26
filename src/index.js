57bab42363c5627d2c78a66dfa64edfcd6238327
SuperFlyXXI <superflyxxi@yahoo.com>
2021-04-26 18:20:21 -0400

    Change output to be json

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-	res.status(200).send('Hello World!');
+	res.status(200).send({ message: 'Hello world!'});
 });
 
 app.listen(port, () => {
 	console.log('Started listinening on ', port);
 });
+
+module.exports = app;
