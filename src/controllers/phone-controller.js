a79b09215c835adcbd57b6fdfbe1371eb0e2a3e1
SuperFlyXXI <superflyxxi@yahoo.com>
2021-04-28 17:04:34 -0400

    Enable 4 week caching

--- src/controllers/phone-controller.js
+++ src/controllers/phone-controller.js
@@ -3,7 +3,7 @@ const model = require('../models/phone-model.js');
 exports.getPhone = async function(req, res) {
 	const phone = await model.getPhone(req.params.id);
 	if (phone) {
-		res.status(200).send(phone);
+		res.set('cache-control', 'public, max-age=2419200').status(200).send(phone);
 	}
 	res.status(404).send();
 }
