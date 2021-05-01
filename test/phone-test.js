33c7811f03e1eb29b88188d57420ad8edb6c173a
SuperFlyXXI <superflyxxi@yahoo.com>
2021-04-30 20:47:07 -0400

    Test phone

---
+++ test/phone-test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../src/index.js');
+const {expect} = chai;
+
+chai.use(chaiHttp);
+
+describe('Phones positive tests', () => {
+	it('Ensure it gets a valid id', (done) => {
+		chai
+			.request(app)
+			.get('/v1/phones/123')
+			.end((error, res) => {
+				expect(res).to.have.status(200);
+				expect(res.body.id).to.equals('123');
+				expect(res.body.name).to.equals('Sample Phone');
+				done();
+			});
+	});
+});
