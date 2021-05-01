6d6bfb0488a3ce9c4228644949ba406f94e3b1f5
SuperFlyXXI <superflyxxi@yahoo.com>
2021-04-30 20:46:26 -0400

    XO formatting

--- test/hello-world-test.js
+++ test/hello-world-test.js
@@ -7,13 +7,13 @@ chai.use(chaiHttp);
 
 describe('Basic test', () => {
 	it('Should get 200', (done) => {
-		chai.request(app)
+		chai
+			.request(app)
 			.get('/')
-			.end((err, res) => {
+			.end((error, res) => {
 				expect(res).to.have.status(200);
-				expect(res.body.message).to.equals("Hello world!");
+				expect(res.body.message).to.equals('Hello world!');
 				done();
 			});
 	});
 });
-
