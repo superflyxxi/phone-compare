becf7ac825a546b55186ad2fcd7cf6cabd7e26f7
SuperFlyXXI <superflyxxi@yahoo.com>
2021-05-02 10:30:53 -0400

    Support reading from file

--- src/models/phone-model.js
+++ src/models/phone-model.js
@@ -10,11 +10,20 @@
  * nfc
  * lineageOSVersion
  */
+const NotFoundError = require('../error-handler/not-found-error.js');
+const rootDir = process.env.DATA_DIR ?? '/data/';
+const fs = require('fs/promises');
 
 exports.getPhone = async function (manufacturer, model) {
-	const gsm = await require('../helpers/gsm-arena.js').getGsmArenaData('https://www.gsmarena.com/google_pixel_5-10386.php');
-	return {manufacturer, model, name: 'Sample Phone', gsmArena: gsm};
+	try {
+		const phone = JSON.parse(await fs.readFile(`${rootDir}${manufacturer}-${model}.json`));
+		phone.gsmArena = await require('../helpers/gsm-arena.js').getGsmArenaData(phone.gsmArenaUrl);
+		return phone;
+	} catch (err) {
+		throw new NotFoundError(err.message);
+	}
 };
 
-exports.savePhone = function (manufacturer, model, phone) {
+exports.savePhone = async function (manufacturer, model, phone) {
+	await fs.writeFile(phone, `${rootDir}${manufacturer}-${model}.json`);
 }
