e13549d1fbddcddae9b094720d4eb9d93cf83c75
SuperFlyXXI <superflyxxi@yahoo.com>
2021-05-03 19:18:27 -0400

    Initial docs

---
+++ src/routers/v1/phone-compare.js
@@ -0,0 +1,99 @@
+const router = require('express').Router();
+// Const controller = require('../../controllers/phone-controller.js');
+// const asyncHandler = require('express-async-handler');
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     PhoneCompareRequestSinglePhone:
+ *       type: object
+ *       properties:
+ *         manufacturer:
+ *           type: string
+ *           description: The phone manufacturer.
+ *           example: LG
+ *         model:
+ *           type: string
+ *           description: The phone model number.
+ *           example: E960
+ *     PhoneCompareRequest:
+ *       type: object
+ *       properties:
+ *         phones:
+ *           type: array
+ *           description: An array of phones to compare. If not provided, then all known phones will be compared.
+ *           items:
+ *             $ref: '#/components/schemas/PhoneCompareRequestSinglePhone'
+ *     PhoneCompareResultSinglePhone:
+ *       allOf:
+ *         - $ref: '#/components/schemas/PhoneCompareRequestSinglePhone'
+ *         - type: object
+ *           properties:
+ *             score:
+ *               type: number
+ *               description: The score given to this phone.
+ *               example: 100.0
+ *             scoreBreakdown:
+ *               type: object
+ *               properties:
+ *                 nfc:
+ *                   type: number
+ *                   description: The score awarded through nfc.
+ *                 height:
+ *                   type: number
+ *                   description: The score awarded through height.
+ *                 fingerprint:
+ *                   type: number
+ *                   description: The score awarded through fingerprint.
+ *     PhoneCompareResult:
+ *       type: object
+ *       properties:
+ *         results:
+ *           type: array
+ *           description: An array of phone results ranked based on settings.
+ *           items:
+ *             $ref: '#/components/schemas/PhoneCompareResultSinglePhone'
+ */
+
+/**
+ * @openapi
+ * /v1/phone-compare:
+ *   post:
+ *     summary: Calculate best phone
+ *     description: |
+ *       Fetches information about the request /v1/phones and based on /v1/settings will determine
+ *       the best phone and why.
+ *     produces:
+ *       - application/json
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/PhoneCompareRequest'
+ *     responses:
+ *       '200':
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/PhoneCompareResult'
+ *             example:
+ *               results:
+ *                 - manufacturer: LG
+ *                   model: E960
+ *                   score: 100
+ *                 - manufacturer: Google
+ *                   model: Pixel 5
+ *                   score: 3000
+ *       default:
+ *         description: All other errors
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
+router.post('/', (req, res) => {
+	res.status(400).send();
+});
