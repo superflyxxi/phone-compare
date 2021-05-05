c8c06e7093a40d82048bcf5ee75af29bc94945ef
SuperFlyXXI <superflyxxi@yahoo.com>
2021-05-05 14:34:25 -0400

    Move to new validation

--- src/models/phone-model.js
+++ src/models/phone-model.js
@@ -2,6 +2,7 @@ const NotFoundError = require('../error-handler/not-found-error.js');
 const MissingMandatoryParametersError = require('../error-handler/missing-mandatory-parameters-error.js');
 const rootDirectory = process.env.DATA_DIR ?? `${process.env.HOME}/data/`;
 const fs = require('fs/promises');
+const {validate} = require('../helpers/validation');
 
 exports.getPhone = async function (manufacturer, model) {
 	let phone;
@@ -20,18 +21,27 @@ exports.getPhone = async function (manufacturer, model) {
 	return phone;
 };
 
+const validationConstraints = {
+	manufacturer: {
+		presence: true
+	},
+	model: {
+		presence: true
+	},
+	gsmArenaUrl: {
+		presence: true
+	},
+	lineageos: {
+		presence: true
+	}
+};
+
 exports.savePhone = async function (phone) {
+	validate(phone, validationConstraints);
 	const manufacturer = phone.manufacturer;
 	const model = phone.model;
-	if (phone.gsmArenaUrl && phone.lineageos) {
-		await fs.writeFile(
-			`${rootDirectory}${manufacturer}-${model}.json`,
-			JSON.stringify(phone)
-		);
-	} else {
-		throw new MissingMandatoryParametersError({
-			gsmArenaUrl: phone.gsmArenaUrl,
-			lineageos: phone.lineageos
-		});
-	}
+	await fs.writeFile(
+		`${rootDirectory}${manufacturer}-${model}.json`,
+		JSON.stringify(phone)
+	);
 };
