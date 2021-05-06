c403de7c31fb150521ee3d2238c8810ebdc50ac1
SuperFlyXXI <superflyxxi@yahoo.com>
2021-05-06 09:27:34 -0400

    Mocked phone API for unit tests

--- test/routers/v1/phones/compare/test.js
+++ test/routers/v1/phones/compare/test.js
@@ -2,44 +2,67 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../../../../../src/index.js');
 const {expect} = chai;
+const nock = require('nock');
 
 chai.use(chaiHttp);
 
 describe('Phone-compare positive tests', () => {
 	before(() => {
-		// Create the phones
+		// Mock the phones
+
 		// pixel 5
-		chai
-			.request(app)
-			.put('/v1/phones/manufacturers/google/models/gd1yq')
-			.send({
+		nock('http://localhost:3000')
+			.get('/v1/phones/manufacturers/google/models/gd1yq')
+			.reply(200, {
+				manufacturer: 'Google',
+				model: 'GD1YQ',
 				name: 'Google Pixel 5',
 				gsmArenaUrl: 'https://www.gsmarena.com/google_pixel_5-10386.php',
-				lineageos: '18.1'
-			})
-			.end();
+				lineageos: '18.1',
+				dimensions: {
+					height: 144.7,
+					width: 70.4,
+					depth: 8
+				},
+				nfc: true,
+				fingerprint: true
+			});
 
 		// Pixel 4
-		chai
-			.request(app)
-			.put('/v1/phones/manufacturers/google/models/g020i')
-			.send({
+		nock('http://localhost:3000')
+			.get('/v1/phones/manufacturers/google/models/g020i')
+			.reply(200, {
+				manufacturer: 'Google',
+				model: 'G020I',
 				name: 'Google Pixel 4',
 				gsmArenaUrl: 'https://www.gsmarena.com/google_pixel_4-9896.php',
-				lineageos: '18.1'
-			})
-			.end();
+				lineageos: '18.1',
+				dimensions: {
+					height: 147.1,
+					width: 68.8,
+					depth: 8.2
+				},
+				nfc: true,
+				fingerprint: true
+			});
 
 		// Nexus 4
-		chai
-			.request(app)
-			.put('/v1/phones/manufacturers/lg/models/e960')
-			.send({
+		nock('http://localhost:3000')
+			.get('/v1/phones/manufacturers/lg/models/e960')
+			.reply(200, {
+				manufacturer: 'LG',
+				model: 'E960',
 				name: 'LG Nexus 4',
 				gsmArenaUrl: 'https://www.gsmarena.com/lg_nexus_4_e960-5048.php',
-				lineageos: '16.1'
-			})
-			.end();
+				lineageos: '16.1',
+				dimensions: {
+					height: 133.9,
+					width: 68.7,
+					depth: 9.1
+				},
+				nfc: true,
+				fingerprint: false
+			});
 	});
 
 	it('Rank on height', (done) => {
