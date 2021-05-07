d92f0408b3e139802869696efc07f1bab02fb707
superflyxxi <40075662+superflyxxi@users.noreply.github.com>
2021-05-07 16:22:44 -0400

    fix(compare): Fixed validations and config for year, ram, and prices (#20)
    * verify everything
    
    * Add some extra debugging
    
    * Base it on configuration

--- src/config/index.js
+++ src/config/index.js
@@ -1,19 +1,34 @@
 const dimensionRules = {
 	type: 'number',
-	scoreMethod: 'FROM_MAX'
+	scoreMethod: 'PREFER_LOW'
+};
+
+const priceRules = {
+	type: 'number',
+	scoreMethod: 'PREFER_LOW'
 };
 
 exports.rankRules = {
 	'dimensions.height': dimensionRules,
 	'dimensions.width': dimensionRules,
 	'dimensions.depth': dimensionRules,
+	ram: {
+		type: 'number',
+		scoreMethod: 'PREFER_HIGH'
+	},
 	nfc: {
 		type: 'boolean',
 		scoreMethod: 'PREFER_TRUE'
 	},
+	'price.usd': priceRules,
+	'price.eur': priceRules,
 	'sensors.fingerprint': {
 		type: 'boolean',
 		scoreMethod: 'PREFER_TRUE'
+	},
+	year: {
+		type: 'number',
+		scoreMethod: 'PREFER_HIGH'
 	}
 };
 
