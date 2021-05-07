9ce30aa95f0fe12edab50f1d58066d90979bc205
SuperFlyXXI <superflyxxi@yahoo.com>
2021-05-07 10:07:40 -0400

    Fixed helper

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,6 +1,6 @@
-exports.cleanupDataDir = function() {
+exports.cleanupDataDir = function () {
 	const fs = require('fs');
-	for (file of fs.readdirSync(process.env.DATA_DIR)) {
+	for (const file of fs.readdirSync(process.env.DATA_DIR)) {
 		fs.unlinkSync(require('path').join(process.env.DATA_DIR, file));
 	}
 };
