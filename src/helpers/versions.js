5e43ec943fae77cedcc52d537b22503807b6a08c
SuperFlyXXI <superflyxxi@yahoo.com>
2021-05-10 21:36:22 -0400

    Don't split on something not set

--- src/helpers/versions.js
+++ src/helpers/versions.js
@@ -22,12 +22,15 @@ function getVersionString(object) {
 }
 
 function getVersionObject(string) {
-	const splt = string.split('.');
-	return {
-		major: splt[0] ? Number.parseInt(splt[0], 10) : undefined,
-		minor: splt[1] ? Number.parseInt(splt[1], 10) : undefined,
-		patch: splt[2] ? Number.parseInt(splt[2], 10) : undefined
-	};
+	if (string) {
+		const splt = string.split('.');
+		return {
+			major: splt[0] ? Number.parseInt(splt[0], 10) : undefined,
+			minor: splt[1] ? Number.parseInt(splt[1], 10) : undefined,
+			patch: splt[2] ? Number.parseInt(splt[2], 10) : undefined
+		};
+	}
+	return {};
 }
 
 export {getVersionObject, getVersionString, getAndroidVersion};
