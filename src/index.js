a3454557285060ca93adcc19a2388688540ce36f
SuperFlyXXI <superflyxxi@yahoo.com>
2021-11-02 10:45:04 -0400

    fix: corrected order of error handling

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import apiDocsRouter from './routers/api-docs/index.js';
 const app = express();
 app.use(express.json());
 app.disable('x-powered-by');
+app.use(morgan('short'));
 
 // APIs
 app.use('/v1/phones', phoneRouter);
@@ -19,12 +20,9 @@ app.use('/api-docs', apiDocsRouter);
 
 // eslint-disable-next-line no-unused-vars
 app.use((req, res, next) => {
-	throw new RouteNotFoundError(req);
+	next(new RouteNotFoundError(req));
 });
-
-app.use(morgan('short'));
 app.use(errorHandler);
-
 app.listen(server.port, () => {
 	console.log('Started version', server.version, 'listening on', server.port);
 });
